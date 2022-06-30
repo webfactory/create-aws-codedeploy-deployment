7e84e00f33075738563e146445c9ba75d5785c6c
Matthias Pigulla <mp@webfactory.de>
2022-06-30 17:40:12 +0200

    Allow overriding the branch name used to look up configuration (#20)
    This PR adds a new `config-name` action input value. It can be used to override the name used to look up the deployment configuration in the `branch_config` section of the appspec.yml file.
    
    By default, the current branch name (`master`, or a PR branch) will be used to find a deployment config, by matching the regular expressions from the `branch_config` section in `appspec.yml`.
    
    Overriding the value makes sense when running the action multiple times within the same job or workflow to create multiple deployments for a single push or PR.

--- index.js
+++ index.js
@@ -12,6 +12,7 @@
     const isPullRequest = payload.pull_request !== undefined;
     const commitId = isPullRequest ? payload.pull_request.head.sha : (payload.head_commit ? payload.head_commit.id : github.context.sha); // like "ec26c3e57ca3a959ca5aad62de7213c562f8c821"
     const branchName = isPullRequest ? payload.pull_request.head.ref : payload.ref.replace(/^refs\/heads\//, ''); // like "my/branch_name"
+    const configLookupName = core.getInput('config-name') || branchName;
 
     const skipSequenceCheck = core.getBooleanInput('skip-sequence-check');
 
@@ -20,6 +21,6 @@
     const runNumber = process.env['github_run_number'] || process.env['GITHUB_RUN_NUMBER'];
 
     try {
-        action.createDeployment(applicationName, fullRepositoryName, branchName, commitId, runNumber, skipSequenceCheck, core);
+        action.createDeployment(applicationName, fullRepositoryName, branchName, configLookupName, commitId, runNumber, skipSequenceCheck, core);
     } catch (e) {}
 })();
